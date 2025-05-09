81b1f459a2aa29add76e9992ccfc05a9cd044142
Muhammad Ali Nizami <kustalics@gmail.com>
2025-05-09 09:54:15 +0500

    Asset Image Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_b20/asset_image.dart';
 
 import 'network_image.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: NetworkImageDemo(),
+      home: AssetImageDemo(),
       // home: Scaffold(
       //   backgroundColor: Colors.blue,
       //   appBar: AppBar(
