ffeed7d91cfe7e1b3381a3e512a8ae1cd926a231
Muhammad Ali Nizami <kustalics@gmail.com>
2025-05-09 09:48:29 +0500

    Network Image Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'network_image.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,87 +12,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          title: Text(
-            "First Screen",
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
-          ),
-          centerTitle: true,
-          leading: Icon(
-            Icons.arrow_back,
-            color: Colors.blue,
-            size: 20,
-          ),
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.blue,
-            ),
-            Icon(
-              Icons.shopping_cart,
-              color: Colors.blue,
-            )
-          ],
-          backgroundColor: Colors.yellow,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              'First Text',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            Text(
-              'Second Text',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.add),
-                Icon(Icons.add),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(Icons.add),
-                    Icon(Icons.add),
-                    Icon(Icons.add),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(Icons.add),
-                        Icon(Icons.add),
-                        Column(
-                          children: [
-                            Icon(Icons.add),
-                            Icon(Icons.add),
-                            Icon(Icons.add),
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                Icon(Icons.add),
-              ],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.blue,
-          child: Icon(Icons.add),
-        ),
-      ),
+      home: NetworkImageDemo(),
+      // home: Scaffold(
+      //   backgroundColor: Colors.blue,
+      //   appBar: AppBar(
+      //     title: Text(
+      //       "First Screen",
+      //       style: TextStyle(
+      //           fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      //     ),
+      //     centerTitle: true,
+      //     leading: Icon(
+      //       Icons.arrow_back,
+      //       color: Colors.blue,
+      //       size: 20,
+      //     ),
+      //     actions: [
+      //       Icon(
+      //         Icons.notifications,
+      //         color: Colors.blue,
+      //       ),
+      //       Icon(
+      //         Icons.shopping_cart,
+      //         color: Colors.blue,
+      //       )
+      //     ],
+      //     backgroundColor: Colors.yellow,
+      //   ),
+      //   body: Column(
+      //     mainAxisAlignment: MainAxisAlignment.start,
+      //     children: [
+      //       Text(
+      //         'First Text',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       SizedBox(
+      //         height: 60,
+      //       ),
+      //       Text(
+      //         'Second Text',
+      //         style: TextStyle(fontSize: 20),
+      //       ),
+      //       SizedBox(
+      //         height: 30,
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //         children: [
+      //           Icon(Icons.add),
+      //           Icon(Icons.add),
+      //           Column(
+      //             crossAxisAlignment: CrossAxisAlignment.start,
+      //             children: [
+      //               Icon(Icons.add),
+      //               Icon(Icons.add),
+      //               Icon(Icons.add),
+      //               Row(
+      //                 crossAxisAlignment: CrossAxisAlignment.start,
+      //                 children: [
+      //                   Icon(Icons.add),
+      //                   Icon(Icons.add),
+      //                   Column(
+      //                     children: [
+      //                       Icon(Icons.add),
+      //                       Icon(Icons.add),
+      //                       Icon(Icons.add),
+      //                     ],
+      //                   )
+      //                 ],
+      //               )
+      //             ],
+      //           ),
+      //           Icon(Icons.add),
+      //         ],
+      //       )
+      //     ],
+      //   ),
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: () {},
+      //     backgroundColor: Colors.blue,
+      //     child: Icon(Icons.add),
+      //   ),
+      // ),
     );
   }
 }
